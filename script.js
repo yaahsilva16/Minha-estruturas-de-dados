0b6c6ae8e797a436429fac02ba46c2a32c49379b
Yasmin da Silva <95134741+yaahsilva16@users.noreply.github.com>
2022-01-19 20:47:22 -0300

    entregando meu exerciciozinho

---
+++ script.js
@@ -0,0 +1,13 @@
+let nome = "Yasmin"
+
+let number = 1000
+
+let ano = 2022 - number
+
+let soma = ano + number
+    console.log(soma)
+
+    console.log("Estou mechendo no console")
+
+let QuartaFeira = true 
+    console.log(typeof QuartaFeira)
